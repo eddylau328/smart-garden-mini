ce710982ca56eaf64db955c48626238ec7748592
Eddy Lau <eddylau0328@gmail.com>
2021-03-15 02:30:06 +0800

    Extract loading spinner from scan wifi page

---
+++ sg-mini-firmware/src/LcdDisplayUI/Components/LoadingSpinner/LoadingSpinner.cpp
@@ -0,0 +1,33 @@
+#include "LoadingSpinner.h"
+
+void LoadingSpinner::setLinkage(PageContent *content) {
+  this->connectContent = content;
+}
+
+void LoadingSpinner::spin(bool isSpin) {
+  this->isEnableSpin = isSpin;
+}
+
+void LoadingSpinner::clear() {
+  connectContent->updateContent(" ", 1);
+}
+
+void LoadingSpinner::updateSpinner() {
+  if (isEnableSpin && millis() - lastLoadingUpdate > 500) {
+    switch(loadingIndex) {
+      case 0:
+        connectContent->updateContent("-", 1);
+        break;
+      case 1:
+        connectContent->setCustomCharacterIndex(CUSTOM_BACKSLASH);
+        break;
+      case 2:
+        connectContent->updateContent("|", 1);
+        break;
+      case 3:
+        connectContent->updateContent("/", 1);
+        break;
+    }
+    loadingIndex = (loadingIndex + 1) % 4;
+  }
+}
