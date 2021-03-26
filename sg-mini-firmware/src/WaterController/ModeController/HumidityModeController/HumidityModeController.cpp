91ddac987e80a0d43a856f0c4431031c2acee278
Eddy Lau <eddylau0328@gmail.com>
2021-03-26 21:39:32 +0800

    init humidity mode

--- sg-mini-firmware/src/WaterController/ModeController/HumidityModeController/HumidityModeController.cpp
+++ sg-mini-firmware/src/WaterController/ModeController/HumidityModeController/HumidityModeController.cpp
@@ -1,6 +1,25 @@
 #include "HumidityModeController.h"
 
 void HumidityModeController::mainLoop(WaterPumpController &waterPump) {
+    // target humidity level is the level you always want to keep your plant at
+    uint8_t targetHumidityLevel = 54;
+    // min, max are the acceptable value for not turn on the water valve
+    uint8_t minHumidityLevel = 45;
+    uint8_t maxHumidityLevel = 65;
+
+    /**
+     *  waterPump - WaterPumpController object that allows you to control the water valve
+     *  
+     *  methods of from the WaterPumpController you may need to use:
+     *  - waterOn(unsigned long duration) : turn the valve on with a duration
+     *  - waterOff() : turn the valve off manually (normally you will not need to use it)
+     *  - getIsWaterPumpOn() : get water pump state to check whether you need to waterOn
+     * 
+     *  e.g. use it like this
+     *  waterPump.waterOn() 
+     */
+
+    /* write your code below here */
 }
 
 /*
