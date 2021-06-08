36ff036bbcf50d940bc4c15eda94d9b28c9d4628
Eddy Lau <eddylau0328@gmail.com>
2021-06-08 21:29:04 +0800

    refactor: clean up .ino script

--- sg-mini-firmware/sg-mini-firmware.ino
+++ sg-mini-firmware/sg-mini-firmware.ino
@@ -1,4 +1,3 @@
-// File datafile;  // for SD card file 
 #include "src/Config/Config.h"
 #include "src/LcdDisplayUI/LcdDisplayUI.h"
 #include "src/LcdDisplayUI/PageControl.h"
@@ -30,28 +29,9 @@ void init() {
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-
-  LOG_SET_LEVEL(DebugLogLevel::ERRORS); // all log is printed
+  LOG_SET_LEVEL(DebugLogLevel::ERRORS);
   init();
-  
-// SD card file name create
-/*  char filename[] = "data00.txt";
-  while(SD.exists(filename)){
-    if(filename[5] = '9'){
-      filename[5] = '0';
-      filename[4]++;
-      }
-    else filename[5]++;
-    }
-  File datafile = SD.open(filename,FILE_WRITE);
-  if (datafile) {
-    Serial.println("SD card initiaseld");
-    datafile.println('The DATA of ' +filename);
-    datafile.println("Time, ,Soil humidity,Soil Temperature, ,Light level,Air temperature,Air humidity,INA226 ,Bus Voltage,Pump current,Shunt power, ,UntraSound distance");
-  }
-*/ 
 }
 
 void loop() {
@@ -60,30 +40,3 @@ void loop() {
   Sensors::mainLoop();
   WaterController::mainLoop();
 }
-
-
-/* void sdcard(){
-  if (datafile){
-   //  datafile.println("Time, ,Soil humidity,Soil Temperature, ,Light level,Air temperature,Air humidity,INA226 ,Bus Voltage,Pump current,Shunt power, ,UntraSound distance")
-   datafile.print(millis()/1000,1);
-   datafile.print(", ,");
-   datafile.print(soilmoisturepercent);
-   datafile.print(",");
-   datafile.print(temperatureC);
-   datafile.print(", ,");
-   datafile.print(lux);
-   datafile.print(",");
-   datafile.print(t);
-   datafile.print(",");
-   datafile.print(h);
-   datafile.print(", ,");
-   datafile.print(busVoltage_V);
-   datafile.print(",");
-   datafile.print(current_mA);
-   datafile.print(",");
-   datafile.print(power_mW);   
-   datafile.print(",");   
-   datafile.print(distance,2);
-     }
-   }
-*/
